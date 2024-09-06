f79f235bdc9ce7cfa0167396c91f4debb57da707
Denys Nazarov <div.itcor@gmail.com>
2024-09-06 16:58:17 +0200

    4 ReqresTest

--- src/test/java/ReqresTest.java
+++ src/test/java/ReqresTest.java
@@ -1,3 +1,6 @@
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -18,9 +21,32 @@ public class ReqresTest {
     public void addParamTest() {
         given().
                 baseUri(url).log().all().
-                when().param("page", 2).get("/api/users").
-                then().log().all().assertThat().statusCode(200);
+        when().param("page", 2).
+                get("/api/users").
+        then().log().all().
+                assertThat().
+                statusCode(200);
     }
 
+    @Test
+    public void headerTest() {
+        given().
+                baseUri(url).log().all().
+                when().param("page", 2).
+                get("/api/users").
+                then().log().all().
+                assertThat().
+                statusCode(200).
+                contentType(ContentType.JSON);
+    }
 
+    @Test
+    public void checkResponseTest() {
+        Response response = given().baseUri(url).log().all().
+                when().get("/api/users");
+
+        System.out.println(response.prettyPrint());
+
+        Assertions.assertTrue(response.body().asString().contains("charles.morris@reqres.in"));
+    }
 }
